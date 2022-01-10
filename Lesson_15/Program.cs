098430aa1ff303d122a2e21730acd6dea6b93f44
Александр Захаров <zaher0861@yandex.ru>
2022-01-10 22:59:15 +0300

    Добавьте файлы проекта.

---
+++ Lesson_15/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество шагов арифметической прогрессии - ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            ArithProgression arithProgression = new ArithProgression(2,2);
+            Console.WriteLine("Шаг прогрессии - {0}", arithProgression.x);
+            arithProgression.setStart();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(arithProgression.GetNext()+" ");
+            }
+            Console.WriteLine();
+            arithProgression.Reset();
+            Console.WriteLine();
+            Console.Write("Введите количество шагов геометрической прогрессии - ");
+            length = Convert.ToInt32(Console.ReadLine());
+            GeomProgression geomProgression = new GeomProgression(2, 3);
+            Console.WriteLine("Шаг прогрессии - {0}", geomProgression.x);
+            geomProgression.setStart();
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(geomProgression.GetNext() + " ");
+            }
+            Console.WriteLine();
+            geomProgression.Reset();
+            Console.ReadKey();
+        }
+    }
+    interface ISeries
+    {
+        void setStart();
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        public int x { get; set; }
+        public int a { get; set; }
+        public int a0 { get; set; }
+        public int GetNext()
+        {
+            return a = a + x;
+        }
+
+        public void Reset()
+        {
+            Console.WriteLine("Выполнен сброс до первоначального значения - {0}",a0);
+            a = a0;
+        }
+
+        public void setStart()
+        {
+            a0 = a;
+            Console.Write("Начальное значение - {0}\nАрифметическая прогрессия - ", a0);
+        }
+        public ArithProgression(int a, int x)
+        {
+            this.a = a;
+            this.x = x;
+        }
+    }
+    class GeomProgression : ISeries
+    {
+        public int x { get; set; }
+        public int a { get; set; }
+        public int a0 { get; set; }
+        public int GetNext()
+        {
+            return a = a * x;
+        }
+
+        public void Reset()
+        {
+            Console.WriteLine("Выполнен сброс до первоначального значения - {0}", a0);
+            a = a0;
+        }
+
+        public void setStart()
+        {
+            a0 = a;
+            Console.Write("Начальное значение - {0}\nГеометрическая прогрессия - ", a0);
+        }
+        public GeomProgression(int a, int x)
+        {
+            this.a = a;
+            this.x = x;
+        }
+    }
+}
---
+++ Lesson_15/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lesson_15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson_15")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("dde5d662-b36d-41ce-877c-51e27f86a099")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
